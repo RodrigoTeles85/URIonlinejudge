42c2e67d01c627783dae6314340fc46236fb5499
RodrigoTeles85 <rodrigo.teles85@gmail.com>
2021-04-08 00:16:06 -0300

    resolvendo um erro

--- challange_1001/script.js
+++ challange_1001/script.js
@@ -1,8 +1,8 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var A = parseInt(lines.shift(10));
-var B = parseInt(lines.shift(9));
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
 
 var soma = (A + B)
 
