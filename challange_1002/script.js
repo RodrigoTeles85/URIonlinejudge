7aa6ef7b08e8ab9a1f895bad8af713580abc41ef
RodrigoTeles85 <rodrigo.teles85@gmail.com>
2021-04-14 23:05:49 -0300

    enviando modificações do problema 1002

--- challange_1002/script.js
+++ challange_1002/script.js
@@ -1,18 +1,10 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-/* var raio = parseFloat(lines.shift());
+var raio = parseFloat(lines.shift());
 
 const pi = 3.14159;
 
 var area = pi * Math.pow(raio, 2);
-
-console.log("A="+ area.toFixed(4)); */
-
-const raio = parseFloat(lines.shift());
-
-const pi = 3.14159;
-
-const area = pi * Math.pow(raio, 2);
 
 console.log("A="+ area.toFixed(4));
